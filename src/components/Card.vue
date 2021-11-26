62241be975426efc233850ee83868ad8d9f37d33
stef_fab@hotmail.it <stef_fab@hotmail.it>
2021-11-26 14:03:46 +0100

    add props

---
+++ src/components/Card.vue
@@ -0,0 +1,50 @@
+<template>
+        <div class="card h-100">  
+            <img :src="image" alt="">
+            <h4>{{ title }}</h4>
+            <h6>{{ subTitle }}</h6>
+            <h6>{{ titleYear }}</h6>
+            <p>{{ descrbition }}</p>
+        </div>
+        
+</template>
+
+<script>
+export default {
+ name: 'Card',
+ props: {
+     image: String,
+     title: String,
+     subTitle: String,
+     titleYear: String,
+     descrbition: String,
+ },
+
+};
+</script>
+
+<style scoped lang="scss">
+.card {
+  background-color: #2e3a46;
+  margin: 1rem;
+  text-align: center;
+  padding: 1rem;
+
+  img {
+    margin-bottom: 2rem;
+  }
+
+  h4,h6,p {
+    background-color: #2e3a46;
+  }
+
+  h4 {
+    font-size: 18px;
+    text-transform: uppercase;
+  }
+  h6 {
+    color: #787c82;
+    padding: 10px 0;
+  }
+}
+</style>
--- src/components/sectionCard.vue
+++ src/components/sectionCard.vue
@@ -1,12 +1,15 @@
 <template>
   <div class="container">
     <div v-if="CardList !== null" class="row">
-      <div class="col-2 card" v-for="(card, index) in CardList" :key="`card-${index}`">
-        <img :src="card.poster" alt="">
-          <h4>{{ card.title }}</h4>
-          <h6>{{ card.author }}</h6>
-          <h6>{{ card.year }}</h6>
-          <p>{{ card.genre }}</p>
+      <div class="col-3 Listcard" v-for="(card, index) in CardList" :key="`card-${index}`">
+        <!-- Card -->
+        <Card 
+          :image="card.poster"
+          :title="card.title"
+          :subTitle="card.author"
+          :titleYear="card.year"
+          :descrbition="card.genre"
+        />
       </div>
     </div>
     <div v-else>loading</div>
@@ -15,9 +18,13 @@
 
 <script>
 import axios from 'axios';
+import Card from '@/components/Card.vue'
 
 export default {
     name: 'sectionCard',
+    components: {
+      Card,
+    },
 
     data() {
       return {
@@ -46,27 +53,8 @@ export default {
 
 
 <style scoped lang="scss">
-.card {
-  background-color: #2e3a46;
-  margin: 1rem;
-  padding: 1.5rem;
-  text-align: center;
-
-  img {
-    margin-bottom: 2rem;
-  }
-
-  h4,h6,p {
-    background-color: #2e3a46;
-  }
-
-  h4 {
-    font-size: 20px;
-    text-transform: uppercase;
-  }
-  h6 {
-    color: #787c82;
-    padding: 10px 0;
-  }
+.Listcard {
+  padding: 1rem;
+  margin-bottom: 1rem;
 }
 </style>
